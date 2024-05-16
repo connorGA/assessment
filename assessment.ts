d59e610442c8c44a9e008940f8e43f0314542c0f
connorGA <ronnoccap@gmail.com>
2024-05-15 18:15:06 -0700

    Question 2 solution

--- assessment.ts
+++ assessment.ts
@@ -19,3 +19,34 @@ export function question1_removeSmallNumbers(list: (number | undefined)[]) {
 
 
 
+/** Question 2. Write classes that implements the following interfaces, assuming that all methods are used with approximately the same frequency.
+ *              Explain why you chose the data structures you used.
+ */
+export interface Item {
+    id: string
+    tag: string
+}
+
+export interface ItemStore {
+    /**
+     * Adds an {@link Item} to the store, replacing any existing item with the
+     * same {@link Item#id} value.
+     */
+    put: (item: Item) => void
+
+    /**
+     * Retrieves the {@link Item} with the given {@link Item#id} value, or
+     * null if no such {@link Item} exists in the store.
+     */
+    get: (id: string) => Item | null
+
+    /**
+     * Delete all {@link Item}s with the specified tag.
+     */
+    dropAllByTag: (tag: string) => void
+
+    /**
+     * Returns the number of Items in the store
+     */
+    size: () => number
+}
